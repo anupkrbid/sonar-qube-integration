c5932b5b2e2676ba82eed3998872a4b26b30cb6f
Anup Kumar Bid <dev.anupkrbid@gmail.com>
2025-05-31 21:32:02 +0530

    introduced unused variables

--- src/other.js
+++ src/other.js
@@ -2,4 +2,6 @@ function greet(name) {
   return `Hello, ${name}!`;
 }
 
+var unusedVariable = "This variable is not used in the code";
+
 module.exports = { greet };
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,4 +6,6 @@ function cube(n) {
   return n * n * n;
 }
 
+var unusedVariable = "This variable is not used in the code";
+
 module.exports = { square, cube };
