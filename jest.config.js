c5d9f9ee4d7cc5adfeac2886b88947488881db7a
Sethupathy Natrayan <sethupathy@spotdraft.com>
2025-06-19 22:51:59 +0530

    remove jest threshhold

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,4 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.js"],
   coverageDirectory: "coverage",
-  coverageThreshold: {
-    "./src/important.js": {
-      // lines: 90,
-      functions: 50,
-      branches: 50,
-      // statements: 90,
-    },
-    "./src/utils/": {
-      // lines: 80,
-      functions: 80,
-      branches: 80,
-      // statements: 80,
-    },
-  },
 };
